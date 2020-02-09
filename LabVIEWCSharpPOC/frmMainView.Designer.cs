0596d3ca583d05feac1448f41dd4a6a0a6787686
masalinas <miguel@thingtrack.com>
2020-02-10 00:26:05 +0100

    last update

---
+++ LabVIEWCSharpPOC/frmMainView.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TresaVisualPOC
+{
+    partial class frmMainView
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLabviewFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.lblLoopRate = new System.Windows.Forms.Label();
+            this.txtLoopRate = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLabviewFile
+            // 
+            this.txtLabviewFile.Location = new System.Drawing.Point(12, 12);
+            this.txtLabviewFile.Name = "txtLabviewFile";
+            this.txtLabviewFile.Size = new System.Drawing.Size(296, 20);
+            this.txtLabviewFile.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(314, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.BackColor = System.Drawing.Color.Gray;
+            this.btnSwitch.Enabled = false;
+            this.btnSwitch.Location = new System.Drawing.Point(12, 82);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(377, 59);
+            this.btnSwitch.TabIndex = 2;
+            this.btnSwitch.Text = "OFF";
+            this.btnSwitch.UseVisualStyleBackColor = false;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // lblLoopRate
+            // 
+            this.lblLoopRate.AutoSize = true;
+            this.lblLoopRate.Location = new System.Drawing.Point(214, 55);
+            this.lblLoopRate.Name = "lblLoopRate";
+            this.lblLoopRate.Size = new System.Drawing.Size(79, 13);
+            this.lblLoopRate.TabIndex = 3;
+            this.lblLoopRate.Text = "Loop Rate (Hz)";
+            // 
+            // txtLoopRate
+            // 
+            this.txtLoopRate.Location = new System.Drawing.Point(306, 52);
+            this.txtLoopRate.Name = "txtLoopRate";
+            this.txtLoopRate.ReadOnly = true;
+            this.txtLoopRate.Size = new System.Drawing.Size(83, 20);
+            this.txtLoopRate.TabIndex = 4;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(12, 52);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // frmMainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 161);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtLoopRate);
+            this.Controls.Add(this.lblLoopRate);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtLabviewFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "frmMainView";
+            this.Text = "Tresa Visual POC";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMainView_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtLabviewFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Label lblLoopRate;
+        private System.Windows.Forms.TextBox txtLoopRate;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ LabVIEWCSharpPOC/frmMainView.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Include LabVIEW namespace from the COM Interface
+using LabVIEW;
+
+namespace TresaVisualPOC
+{
+    public partial class frmMainView : Form
+    {
+        private string labViewFile;
+        private _Application labViewApp;
+        private VirtualInstrument viApp;
+
+        private void InitLabviewApplication()
+        {
+            try
+            {
+                this.labViewApp = new LabVIEW.Application();
+
+                if (this.labViewApp == null)
+                {
+                    Console.WriteLine("TresaPOC : An error occurred getting labVIEWApp.");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        private void StartViApplication()
+        {
+            //Create VI object
+            //this.viApp = labViewApp.GetVIReference(this.labViewFile, "", true, 0);
+            this.viApp = labViewApp.GetVIReference(this.labViewFile);
+
+            if (this.viApp == null)
+            {
+                Console.WriteLine("TresaPOC : An error occurred getting the VI reference.  Make sure the VI path you entered is correct.");
+                return;
+            }
+
+            //Create input parameter names object to pass to the VI
+            /*object paramNames = new object[]
+            {
+                    "Serial Port",             //input Arduino serial port configuiration
+                    "Digital Output Channel",  //input Arduino digital pin
+                    "LED Value",               //input Arduino Switch ON/OFF Button
+                    "Stop Button"              //input Arduino Stop Button
+            };
+
+            //Create input parameter values to pass to the VI
+            object paramValues = new object[]
+            {
+                    "COM3", //input Arduino serial port configuiration
+                    13,     //input Arduino digital pin
+                    0,      //input Arduino Switch ON/OFF Button
+                    0       //input Arduino Stop Button
+            };
+
+            // Call the VI with the "Call2" method. "Call2" is very similar to "Call" except "Call2"
+            // has a few more common input parameters. Both are like calling a subVI or a function call
+            // while the "Run" method is more like hitting the run button on a VI. Also, parameters
+            // are passed in and out by way of the first two parameters in the call method.
+
+            //Call VI and provide input parameters
+            this.viApp.Call2(ref paramNames, ref paramValues, true, false, false, true);*/
+
+            this.viApp.FPWinOpen = true;
+
+            this.viApp.SetControlValue("Serial Port", "COM3");
+            this.viApp.SetControlValue("Digital Output Channel", 13);
+            this.viApp.SetControlValue("LED Value", 0);
+            this.viApp.SetControlValue("Stop Button", 0);
+
+            //Run the VI with default input values, you will see the values on the front panel update if it is open
+            this.viApp.Run(true);
+        }
+
+        public frmMainView()
+        {
+            InitializeComponent();
+
+            // Initialize Labview Application
+            InitLabviewApplication();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdLabbiewFile = new OpenFileDialog
+            {
+                InitialDirectory = Path.Combine(Directory.GetCurrentDirectory(), "labview"),
+                Title = "Browse Labview Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "vi",
+                Filter = "Labview files (*.vi)|*.vi",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (ofdLabbiewFile.ShowDialog() == DialogResult.OK)
+            {
+                // show vi file selected
+                this.labViewFile = ofdLabbiewFile.FileName;
+
+                // set vi file selected
+                txtLabviewFile.Text = Path.GetFileName(this.labViewFile);
+
+                // enable switch button
+                btnSwitch.Enabled = true;
+
+                // start vi labview application
+                StartViApplication();
+            }
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            if (btnSwitch.Text == "OFF")
+            {
+                btnSwitch.Text = "ON";
+                btnSwitch.BackColor = Color.Green;
+
+                this.viApp.SetControlValue("LED Value", 1);
+            } else
+            {
+                btnSwitch.Text = "OFF";
+                btnSwitch.BackColor = Color.Gray;
+
+                this.viApp.SetControlValue("LED Value", 0);
+            }
+
+            txtLoopRate.Text = this.viApp.GetControlValue("Loop Rate (Hz)").ToString();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            this.viApp.SetControlValue("Stop Button", 1);
+        }
+
+        private void frmMainView_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // stop vi labview application
+            this.viApp.SetControlValue("Stop Button", 1);
+
+            // close labview runtime
+            this.labViewApp.Quit();
+        }
+    }
+}
